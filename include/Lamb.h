0d2e4daa4093c345e4361233a6423c9f5d1ec120
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2019-01-02 18:30:10 +0000

    Lamb.h/cpp: added AMReX_Config header which includes spacedim, added hydrovar arrays and constructor, destructor

--- include/Lamb.h
+++ include/Lamb.h
@@ -1,6 +1,9 @@
 #ifndef LAMB_H
 #define LAMB_H
 
+#include "AMReX_Config.H"
+#include "AMReX_Box.H"
+
 class Lamb {
 private:
   // physical dimensions of the outer domain
@@ -8,6 +11,7 @@ private:
   const int _NX;
   const int _NY;
   const int _NZ;
+  const int _NUMEL = _NX * _NY * _NZ;
 
   // relaxation times
   const double _TAU_S; // shear
@@ -19,8 +23,17 @@ private:
   // current time step
   int time_step = 0;
 
+  // distribution function
+  amrex::Box dist_f;
+
+  // hydrodynamic variables (output arrays)
+  double * density;
+  double * velocity;
+  double * force;
+
 public:
   Lamb(int, int, int, double, double);
+  ~Lamb();
 };
 
 #endif
--- src/Lamb.cpp
+++ src/Lamb.cpp
@@ -1,3 +1,17 @@
 #include "Lamb.h"
 
-Lamb::Lamb(int nx, int ny, int nz, double tau_s, double tau_b) : _NX(nx), _NY(ny), _NZ(nz), _TAU_S(tau_s), _TAU_B(tau_b) {};
+Lamb::Lamb(int nx, int ny, int nz, double tau_s, double tau_b) : _NX(nx), _NY(ny), _NZ(nz), _TAU_S(tau_s), _TAU_B(tau_b) {
+  density = new double[_NUMEL];
+  velocity = new double[_NUMEL];
+  force = new double[_NUMEL];
+
+  return;
+};
+
+Lamb::~Lamb() {
+  delete[] density;
+  delete[] velocity;
+  delete[] force;
+
+  return;
+}
