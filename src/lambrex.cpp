b30e59e29de9b677b8e2aeca8ae3786b303ade82
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2019-01-03 21:49:02 +0000

    lambrex.h/cpp: added periodicity to lambrexInit to be passed forward to Lamb constructor

--- src/lambrex.cpp
+++ src/lambrex.cpp
@@ -2,12 +2,12 @@
 #include "AMReX.H"
 #include "lambrex.h"
 
-Lamb * lambrexInit(int argc, char ** argv, int nx, int ny, int nz, double tau_s, double tau_b) {
+Lamb * lambrexInit(int argc, char ** argv, int nx, int ny, int nz, double tau_s, double tau_b, int * periodicity) {
   printf("Initialising LAMBReX\n");
 
   amrex::Initialize(argc, argv, false);
 
-  Lamb * lbrx = new Lamb(nx, ny, nz, tau_s, tau_b);
+  Lamb * lbrx = new Lamb(nx, ny, nz, tau_s, tau_b, periodicity);
   printf("A lamb was born!\n");
 
   return lbrx;
