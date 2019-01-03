a814a0115ae5020a2bfdb91514b433bfd2172619
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2019-01-03 21:51:22 +0000

    Lamb.h/cpp: added periodicity and AMReX boxes and Geometry

--- include/Lamb.h
+++ include/Lamb.h
@@ -3,36 +3,44 @@
 
 #include "AMReX_Config.H"
 #include "AMReX_Box.H"
+#include "AMReX_RealBox.H"
+#include "AMReX_Geometry.H"
 
 class Lamb {
 private:
   // physical dimensions of the outer domain
-  const int _NDIMS = 3;
+  const int _NDIMS = AMREX_SPACEDIM;
   const int _NX;
   const int _NY;
   const int _NZ;
   const int _NUMEL = _NX * _NY * _NZ;
+  const int COORD_SYS = 0;
+  int _periodicity[AMREX_SPACEDIM];
 
   // relaxation times
   const double _TAU_S; // shear
   const double _TAU_B; // bulk
 
   // speed of sound squared
-  const double _CS2 = 1.0 / 3.0;
+  const double CS2 = 1.0 / 3.0;
 
   // current time step
   int time_step = 0;
 
-  // distribution function
-  amrex::Box dist_f;
+  //  AMReX domain specification
+  amrex::Box idx_domain;
+  amrex::RealBox phys_domain;
+  amrex::Geometry geometry;
 
   // hydrodynamic variables (output arrays)
   double * density;
   double * velocity;
   double * force;
 
+  void buildGeo();
+
 public:
-  Lamb(int, int, int, double, double);
+  Lamb(int, int, int, double, double, int *);
   ~Lamb();
 };
 
--- src/Lamb.cpp
+++ src/Lamb.cpp
@@ -1,9 +1,36 @@
 #include "Lamb.h"
+#include "AMReX_IntVect.H"
+#include "AMReX_IndexType.H"
+#include "AMReX_Box.H"
 
-Lamb::Lamb(int nx, int ny, int nz, double tau_s, double tau_b) : _NX(nx), _NY(ny), _NZ(nz), _TAU_S(tau_s), _TAU_B(tau_b) {
+void Lamb::buildGeo() {
+  const amrex::IntVect lo_corner( AMREX_D_DECL(0, 0, 0) );
+  const amrex::IntVect hi_corner( AMREX_D_DECL(_NX-1, _NY-1, _NZ-1) );
+  const amrex::IndexType idx_type( {AMREX_D_DECL(0, 0, 0)} );
+
+  idx_domain.setSmall(lo_corner);
+  idx_domain.setBig(hi_corner);
+  idx_domain.setType(idx_type);
+
+  phys_domain.setLo( {AMREX_D_DECL(0.0, 0.0, 0.0)} );
+  phys_domain.setHi( {AMREX_D_DECL((double) _NX-1, (double) _NY-1, (double) _NZ-1)} );
+
+  geometry.define(idx_domain, &phys_domain, COORD_SYS, _periodicity);
+
+  return;
+}
+
+Lamb::Lamb(int nx, int ny, int nz, double tau_s, double tau_b, int * periodicity)
+: _NX(nx), _NY(ny), _NZ(nz), _TAU_S(tau_s), _TAU_B(tau_b) {
+  for (int dim = 0; dim < _NDIMS; ++dim) {
+    _periodicity[dim] = periodicity[dim];
+  }
+
   density = new double[_NUMEL];
   velocity = new double[_NUMEL];
   force = new double[_NUMEL];
+
+  buildGeo();
 
   return;
 };
