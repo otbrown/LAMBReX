c4ffc62b91c2b33c26d4940d57e0ee869a52f428
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2019-01-18 16:10:02 +0000

    pulse.cpp: new/delete replaced with unique_ptr, no lambrexInit/Finalize

--- examples/pulse.cpp
+++ examples/pulse.cpp
@@ -1,21 +1,22 @@
 #include <cstdio>
+#include <memory>
 #include "lambrex.h"
 
 int main (int argc, char * argv[])
 {
-  int nx = 10;
-  int ny = 10;
-  int nz = 50;
-  double tau = 0.5;
+  const int nx = 10;
+  const int ny = 10;
+  const int nz = 50;
+  const double tau = 0.5;
   int periodicity[3] = {1, 1, 1};
-  int numel = nx * ny * nz;
+  const int numel = nx * ny * nz;
   double amplitude = 0.01;
   int i, j, k;
   double z_mean;
 
   // create initial density array
-  double * rho = new double[numel];
-  for (i = 0; i < numel; ++i) rho[i] = 1.0;
+  double rho[numel] = {1.0};
+
   // set enhanced density
   for (i = 0; i < nx; ++i) {
     for (j = 0; j < ny; ++j) {
@@ -28,20 +29,22 @@ int main (int argc, char * argv[])
   z_mean /= nz;
   for (i = 0; i < numel; ++i) rho[i] /= z_mean;
 
-  Simulation * lbrx = lambrexInit(argc, argv, nx, ny, nz, tau, tau, periodicity);
+  amrex::Initialize(argc, argv, false);
+  {
+    const std::unique_ptr<Simulation> lbrx =
+      std::make_unique<Simulation>(nx, ny, nz, tau, tau, periodicity);
 
-  // provide LAMBReX with initial density and velocity
-  // density is copied so safe to free
-  lbrx->setDensity(rho);
-  printf("Density initialised.\n");
-  lbrx->setVelocity(0.0);
-  printf("Velocity initialised.\n");
-  delete[] rho;
+    // provide LAMBReX with initial density and velocity
+    // density is copied so safe to free
+    lbrx->setDensity(rho);
+    printf("Density initialised.\n");
+    lbrx->setVelocity(0.0);
+    printf("Velocity initialised.\n");
 
-  lbrx->calcEquilibriumDist();
-  printf("Equilibrium distribution calculated.\n");
-
-  lambrexFinalize(lbrx);
+    lbrx->calcEquilibriumDist();
+    printf("Equilibrium distribution calculated.\n");
+  }
+  amrex::Finalize();
 
   return 0;
 }
