6599d814152676250fd49384b8a17ba2c08752ec
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2019-02-25 16:08:34 +0000

    catch2Main.cpp: added AMReXListener to initialize and finalize AMReX (and MPI) at beginning and end of test run

--- tests/catch2Main.cpp
+++ tests/catch2Main.cpp
@@ -1,2 +1,28 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
+#include "lambrex.h"
+
+#define CATCH_CONFIG_EXTERNAL_INTERFACES
+
+struct AMReXListener : Catch::TestEventListenerBase {
+
+  using TestEventListenerBase::TestEventListenerBase; // inherit constructor
+
+  // Get rid of Wweak-tables
+  ~AMReXListener();
+
+  virtual void testRunStarting(Catch::TestRunInfo const& testRunInfo) override {
+    int argc = 0;
+    char **argv;
+    amrex::Initialize(argc, argv, false);
+  }
+
+  virtual void testRunEnded(Catch::TestRunStats const& testRunStats) override {
+    amrex::Finalize();
+  }
+};
+
+CATCH_REGISTER_LISTENER(AMReXListener)
+
+// Get rid of Wweak-tables
+AMReXListener::~AMReXListener() {}
