f82a0e0a7b449c29986d5fba400b48a162cdcec9
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2019-02-25 16:10:14 +0000

    catch2InitTests.cpp: AMReX now initialized and finalized elsewhere, added second TEST_CASE

--- tests/catch2InitTests.cpp
+++ tests/catch2InitTests.cpp
@@ -5,20 +5,16 @@
 
 TEST_CASE("Single Value Initialisation", "[initialisation]")
 {
-  int argc = 0;
-  char **argv;
   const double TEST_DENSITY = 0.63;
   const int TEST_NX = 11;
   const int TEST_NY = 12;
   const int TEST_NZ = 13;
   int periodicity[3] = {1,1,1};
 
-  amrex::Initialize(argc, argv, false);
+  Simulation sim(TEST_NX, TEST_NY, TEST_NZ, 0.01, 0.01, periodicity);
+  sim.setDensity(TEST_DENSITY);
 
   SECTION("check dimensions") {
-    Simulation sim(TEST_NX, TEST_NY, TEST_NZ, 0.01, 0.01, periodicity);
-    sim.setDensity(TEST_DENSITY);
-
     std::array<int,3> dims = sim.getDims();
     REQUIRE(dims[0] == TEST_NX);
     REQUIRE(dims[1] == TEST_NY);
@@ -26,9 +22,6 @@ TEST_CASE("Single Value Initialisation", "[initialisation]")
   }
 
   SECTION("check density values") {
-    Simulation sim(TEST_NX, TEST_NY, TEST_NZ, 0.01, 0.01, periodicity);
-    sim.setDensity(TEST_DENSITY);
-
     for (int k = 0; k < TEST_NZ; ++k) {
       for (int j = 0; j < TEST_NY; ++j) {
         for (int i = 0; i < TEST_NX; ++i) {
@@ -37,6 +30,41 @@ TEST_CASE("Single Value Initialisation", "[initialisation]")
       }
     }
   }
+}
 
-  amrex::Finalize();
+TEST_CASE("Element-wise Initialisation", "[initialisation]")
+{
+  const int TEST_NX = 16;
+  const int TEST_NY = 9;
+  const int TEST_NZ = 8;
+  int periodicity[3] = {1,1,1};
+
+  Simulation sim(TEST_NX, TEST_NY, TEST_NZ, 0.01, 0.01, periodicity);
+
+  double val = 0.0;
+  for (int k = 0; k < TEST_NZ; ++k) {
+    for (int j = 0; j < TEST_NY; ++j) {
+      for (int i = 0; i < TEST_NX; ++i) {
+        sim.setDensity(i, j, k, val++);
+      }
+    }
+  }
+
+  SECTION("check dimensions") {
+    std::array<int,3> dims = sim.getDims();
+    REQUIRE(dims[0] == TEST_NX);
+    REQUIRE(dims[1] == TEST_NY);
+    REQUIRE(dims[2] == TEST_NZ);
+  }
+
+  SECTION("check density values") {
+    val = 0.0;
+    for (int k = 0; k < TEST_NZ; ++k) {
+      for (int j = 0; j < TEST_NY; ++j) {
+        for (int i = 0; i < TEST_NX; ++i) {
+          REQUIRE(sim.getDensity(i, j, k) == Approx(val++));
+        }
+      }
+    }
+  }
 }
